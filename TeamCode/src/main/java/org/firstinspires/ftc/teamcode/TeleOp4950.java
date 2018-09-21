39ff57b31846c4585513a6302988684b028a6406
Eric Hu <eric.zhang.hu.2002@gmail.com>
2018-09-21 10:56:09 -0700

    deleted servo part because the team does not have servo arm yet?

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp4950.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp4950.java
@@ -3,20 +3,12 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.Servo;
 
 public class TeleOp4950 extends LinearOpMode{
 
     // Declare drive motors
     private DcMotor motorRight;
     private DcMotor motorLeft;
-
-    // declare the arm variable
-    private Servo armServo;
-
-    // To move robotic arm
-    private static final double ARM_RETRACTED_POSITION = 0.2;
-    private static final double ARM_EXTENDED_POSITION = 0.8;
 
     public void runOpMode() throws InterruptedException
     {
@@ -28,12 +20,6 @@ public class TeleOp4950 extends LinearOpMode{
         // opposite directions. So reverse one
         motorLeft.setDirection(DcMotor.Direction.REVERSE);
 
-        // Initialize arm servo
-        armServo = hardwareMap.servo.get("armServo");
-
-        // Retract arm servo to stay within 18"
-        armServo.setPosition(ARM_RETRACTED_POSITION);
-
         // Wait until start button is pressed
         waitForStart();
 
@@ -43,16 +29,6 @@ public class TeleOp4950 extends LinearOpMode{
             // Tank drive
             motorLeft.setPower(-gamepad1.left_stick_y);
             motorRight.setPower(-gamepad1.right_stick_y);
-
-            // Move the arm servo if requested
-            if(gamepad2.a)
-            {
-                armServo.setPosition(ARM_EXTENDED_POSITION);
-            }
-            if(gamepad2.b)
-            {
-                armServo.setPosition(ARM_RETRACTED_POSITION);
-            }
 
             // Give hardware a chance to catch up
             idle();
