07c2e2c005f9ba889c339914584e16fcc1aafdbc
Eric Hu <eric.zhang.hu.2002@gmail.com>
2018-10-09 15:56:18 -0700

    tele op2

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp4950.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp4950.java
@@ -1,37 +1,29 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 public class TeleOp4950 extends LinearOpMode{
 
-    // Declare drive motors
-    private DcMotor motorRight;
-    private DcMotor motorLeft;
+    // Declare OpMode variables
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor leftMotor = null;
+    private DcMotor rightMotor = null;
 
-    public void runOpMode() throws InterruptedException
-    {
-        // Initialize drive motors
-        motorLeft = hardwareMap.dcMotor.get("motorLeft");
-        motorRight = hardwareMap.dcMotor.get("motorRight");
+    public void runOpMode() {
+        leftMotor = hardwareMap.dcMotor.get("left_m");
+        rightMotor = hardwareMap.dcMotor.get("right_m");
 
-        // If drive motors are given full power, robot would spin because of the motors being in
-        // opposite directions. So reverse one
-        motorLeft.setDirection(DcMotor.Direction.REVERSE);
-
-        // Wait until start button is pressed
         waitForStart();
 
-        // Repeatedly run code in here until stop button is pressed
-        while(opModeIsActive())
-        {
-            // Tank drive
-            motorLeft.setPower(-gamepad1.left_stick_y);
-            motorRight.setPower(-gamepad1.right_stick_y);
-
-            // Give hardware a chance to catch up
-            idle();
+        while(opModeIsActive()){
+            float leftPower = -gamepad1.left_stick_y;
+            float rightPower = -gamepad1.right_stick_y;
+            leftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+            leftMotor.setPower(leftPower);
+            rightMotor.setPower(rightPower);
         }
     }
 }
